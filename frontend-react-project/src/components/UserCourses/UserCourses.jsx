1e562d1bd49d78ed5852e05eb0cf866063ec793e
p.sereda <piotr.sereda@dentsu.com>
2023-08-23 13:55:15 +0200

    feat Implement UserCourses component

---
+++ frontend-react-project/src/components/UserCourses/UserCourses.jsx
@@ -0,0 +1,27 @@
+import React, { useContext } from 'react';
+import bemCssModules from 'bem-css-modules';
+import Course from '../Course';
+
+import { StoreContext } from '../../store/StoreProvider';
+
+import { default as UserCoursesStyles } from './UserCourses.module.scss';
+
+const block = bemCssModules(UserCoursesStyles)
+
+const UserCourses = () => {
+  const { user, courses } = useContext(StoreContext);
+
+  const buyedCourses = courses.filter(course => user.courses.includes(course.id))
+  .map(course => <Course key={course.id} {...course} />);
+ 
+  return(
+    <section className={block()}>
+      <h2 className={block('title')}>Twoje wykupione kursy</h2>
+      <ul className={block('list')}>
+        { buyedCourses }
+      </ul>
+    </section>
+  )
+}
+
+export default UserCourses;
